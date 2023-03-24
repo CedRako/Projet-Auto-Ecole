c40b50f48a837839fde4b6846cde6e301e1fd102
nathanSIO <janvnathan1@gmail.com>
2023-03-24 10:52:21 +0100

    MAJ Graph

--- src/Vues/FrmEleveGraphique.java
+++ src/Vues/FrmEleveGraphique.java
@@ -91,7 +91,47 @@ public class FrmEleveGraphique extends javax.swing.JFrame {
         for (String valeur : ctrlGraphique.GetDatasGraphiqueEleveLecon(eleve.getNumEleve()).keySet())
         {
             nombreDeLecon = ctrlGraphique.GetDatasGraphiqueEleveLecon(eleve.getNumEleve()).get(valeur);
-            mois = valeur;
+            switch (valeur) {
+                case "1": 
+                    mois = "Janvier";
+                    break;
+                case "2":
+                    mois = "Février";
+                    break;
+                case "3":
+                    mois = "Mars";
+                    break;
+                case "4":
+                    mois = "Avril";
+                    break;
+                case "5":
+                    mois = "Mai";
+                    break;
+                case "6":
+                    mois = "Juin";
+                    break;
+                case "7":
+                    mois = "Juillet";
+                    break;
+                case "8":
+                    mois = "Août";
+                    break;
+                case "9":
+                    mois = "Septembre";
+                    break;
+                case "10":
+                    mois = "Octobre";
+                    break;
+                case "11":
+                    mois = "Novembre";
+                    break;
+                case "12":
+                    mois = "Décembre";
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+            //mois = valeur;
             donnees.setValue(nombreDeLecon,"",mois);
         }
         JFreeChart chart1 = ChartFactory.createBarChart(
