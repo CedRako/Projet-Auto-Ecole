139c2d8b7aa54503354798b1f889ca271229503a
CedRako <cedricecb@hotmail.fr>
2023-03-24 14:51:26 +0100

    Changement formulaire CA

--- src/Tools/ConnexionBDD.java
+++ src/Tools/ConnexionBDD.java
@@ -29,8 +29,8 @@ public class ConnexionBDD
             // chargement du pilote
             Class.forName(pilote);
             // L'objet connexion à la BDD avec le nom de la base, le user et le password
-            cnx = DriverManager.getConnection("jdbc:mysql://localhost/bloc2_java_projet_autoecole?serverTimezone="
-                + TimeZone.getDefault().getID(), "root", "root");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost/autobra?serverTimezone="
+                + TimeZone.getDefault().getID(), "root", "");
         }
         catch (ClassNotFoundException ex)
         {
